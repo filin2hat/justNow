57b82dbbb37e5549367dc867ebce39093e5b213c
Dmitriy Biryulin AN-32 <filin2hat@yandex.ru>
2022-06-15 16:19:59 +0300

    add fun agoToText to main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,20 @@
 fun main() {
-
+    val secondsAgo = 365
+    println(agoToText(secondsAgo))
 }
 
 fun agoToText(secondsAgo: Int): String = when (secondsAgo) {
-    in 0..60 -> "был(а) $secondsAgo назад"
-    //todo
+    in 0..60 -> "был(а) только что."
+    in 61..3600 -> "был(а) ${minutesToText(secondsAgo)} назад."
+    in 3601..86400 -> "был(а) ${hoursToString(secondsAgo)} назад."
+    in 86401..172800 -> "был(а) сегодня"
+    in 172801..259200 -> "был(а) вчера"
     else -> "был(а) давно"
 }
 
 fun minutesToText(seconds: Int): String {
     val minutes = seconds / 60
-    if (minutes >= 11 || minutes <= 14)
+    if (minutes in 11..14)
         return "$minutes минут"
     return when (minutes % 10) {
         1 -> "$minutes минуту"
@@ -20,8 +24,8 @@ fun minutesToText(seconds: Int): String {
 }
 
 fun hoursToString(seconds: Int): String {
-    val hours = seconds / (60 * 60)
-    if (hours >= 5 || hours <= 20)
+    val hours = seconds / 3600
+    if (hours in 5..20)
         return "$hours часов"
     return when (hours % 10) {
         1 -> "$hours час"
