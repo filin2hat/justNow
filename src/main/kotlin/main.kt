daac8807c4fce6f36a3fb27ebc0fbda9e312e934
Dmitriy Biryulin AN-32 <filin2hat@yandex.ru>
2022-06-15 16:02:03 +0300

    add fun hoursToText to main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,11 +14,18 @@ fun minutesToText(seconds: Int): String {
         return "$minutes минут"
     return when (minutes % 10) {
         1 -> "$minutes минуту"
-        2,3,4 -> "$minutes минуты"
+        2, 3, 4 -> "$minutes минуты"
         else -> "$minutes минут"
     }
 }
 
 fun hoursToString(seconds: Int): String {
-
+    val hours = seconds / (60 * 60)
+    if (hours >= 5 || hours <= 20)
+        return "$hours часов"
+    return when (hours % 10) {
+        1 -> "$hours час"
+        2, 3, 4 -> "$hours часа"
+        else -> "$hours часов"
+    }
 }
